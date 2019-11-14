f07416d221fe94d5db75156f295d7f1c9bb1d47f
yishai <yishairasowsky@gmail.com>
2019-11-14 19:31:26 +0200

    Update create_json_data.py

--- examples/training/create_json_data.py
+++ examples/training/create_json_data.py
@@ -3,18 +3,39 @@ from spacy import load
 from spacy.gold import docs_to_json
 nlp = load("en_core_web_sm")
 
+# from tika import parser
+# # print("imported tika!!!!")
+# texts = []
+# fnames = []
+# fname = '(R)LNJ23798-SLA-001-.pdf'
+# file_data = parser.from_file(fname) # Parse data from file
+# text = file_data['content'] # Get files text content
+
+# # import tika
+from tika import parser
+# print("imported tika!!!!")
+fname = 'LNY00157_-_Short_Form_Lease_Redacted.pdf'
+file_data = parser.from_file(fname) # Parse data from file
+my_text = file_data['content'][913:] # Get files text content
+
+texts = []
+texts.append(my_text)
+
 # naming of files
-fname = 'validation'
+fname = 'example'
 txt_file = fname + '.txt'
 json_file = fname + '.json'
 
-f = open(txt_file) # will scrape strings from here
-lines = f.readlines()  # list of srings from the txt file
+# f = open(txt_file) # will scrape strings from here
+# lines = f.readlines()  # list of srings from the txt file
 
 docs = [] # initialize a list to be populated wih nlp doc objects
-for line in lines:
+# for line in lines:
     # print(line[:]) # display the sentence from that line
-    doc = nlp(line) # convert string into a spacy doc object using nlp
+for text in texts:
+
+    doc = nlp(text) # convert string into a spacy doc object using nlp
+    # doc = nlp(line) # convert string into a spacy doc object using nlp
     docs.append(doc) # add new doc to the list of docs
 
 json_data = docs_to_json(docs) # convert doc into a json file
@@ -24,4 +45,5 @@ json_data = docs_to_json(docs) # convert doc into a json file
 #     json.dump(json_data, outfile)
 
 import srsly
-srsly.write_json('json/' + json_file, [spacy.gold.docs_to_json(docs)])
+# srsly.write_json('json/' + json_file, [spacy.gold.docs_to_json(docs)])
+srsly.write_json(json_file, [spacy.gold.docs_to_json(docs)])
