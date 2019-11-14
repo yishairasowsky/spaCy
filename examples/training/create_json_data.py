70ec6df957e467d66361a4070fa7a78d548ab3f7
yishai <yishairasowsky@gmail.com>
2019-11-14 11:31:45 +0200

    Update create_json_data.py
    create json file of real data

--- examples/training/create_json_data.py
+++ examples/training/create_json_data.py
@@ -2,17 +2,25 @@ import spacy
 from spacy import load 
 from spacy.gold import docs_to_json
 nlp = load("en_core_web_sm")
-f = open('train.txt')
-docs = [] # initialize a list to be populated wih strings
-lines = f.readlines()  # returns a list of srings from the txt file
+
+# naming of files
+fname = 'real_data'
+txt_file = fname + .'.txt'
+json_file = fname + '.json'
+
+f = open(txt_file) # will scrape strings from here
+lines = f.readlines()  # list of srings from the txt file
+
+docs = [] # initialize a list to be populated wih nlp doc objects
 for line in lines:
-    # print(line[:]) # display the sentence from that line
+    print(line[:]) # display the sentence from that line
     doc = nlp(line) # convert string into a spacy doc object using nlp
     docs.append(doc) # add new doc to the list of docs
+
 json_data = docs_to_json(docs) # convert doc into a json file
 
 import json
-with open('train.json', 'w+') as outfile:
+with open(json_file, 'w+') as outfile:
     json.dump(json_data, outfile)
 
 # import srsly
