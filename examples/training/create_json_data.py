d9ccdc0faabff8a603244891987633802e32f2e8
yishai <yishairasowsky@gmail.com>
2019-11-14 12:21:22 +0200

    train on examples sentences of ddates

--- examples/training/create_json_data.py
+++ examples/training/create_json_data.py
@@ -4,8 +4,8 @@ from spacy.gold import docs_to_json
 nlp = load("en_core_web_sm")
 
 # naming of files
-fname = 'real_data'
-txt_file = fname + .'.txt'
+fname = 'validation'
+txt_file = fname + '.txt'
 json_file = fname + '.json'
 
 f = open(txt_file) # will scrape strings from here
@@ -13,15 +13,15 @@ lines = f.readlines()  # list of srings from the txt file
 
 docs = [] # initialize a list to be populated wih nlp doc objects
 for line in lines:
-    print(line[:]) # display the sentence from that line
+    # print(line[:]) # display the sentence from that line
     doc = nlp(line) # convert string into a spacy doc object using nlp
     docs.append(doc) # add new doc to the list of docs
 
 json_data = docs_to_json(docs) # convert doc into a json file
 
-import json
-with open(json_file, 'w+') as outfile:
-    json.dump(json_data, outfile)
+# import json
+# with open('json/' + json_file, 'w+') as outfile:
+#     json.dump(json_data, outfile)
 
-# import srsly
-# srsly.write_json('real_data.json', [spacy.gold.docs_to_json(docs)])
+import srsly
+srsly.write_json('json/' + json_file, [spacy.gold.docs_to_json(docs)])
